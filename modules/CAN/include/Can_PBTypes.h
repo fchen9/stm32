d28127fb7d8b330752c4807e3e2b1cdddaed6313
DungvNg <nvdung694@gmail.com>
2019-09-24 19:28:56 +0700

    kasd

--- modules/CAN/include/Can_PBTypes.h
+++ modules/CAN/include/Can_PBTypes.h
@@ -8,9 +8,11 @@
 #ifndef CAN_PBTYPES_H
 #define CAN_PBTYPES_H
 
+#include "Can_Types.h"
+
 typedef struct Can_FdBaudrateConfigType_STag
 {
-  uint16 usFdBaudrateBRP
+  uint16 usFdBaudrateBRP;
   uint8  ucFdBaudratePropSeg;
   uint8  ucFdBaudrateSeg1;
   uint8  ucFdBaudrateSeg2;
@@ -18,16 +20,17 @@ typedef struct Can_FdBaudrateConfigType_STag
   uint8  ucFdBaudrateSspOffset;
   uint8  ucFdBaudrateTxBRS;
 } Can_FdBaudrateConfigType;
+
 typedef struct Can_BaudrateConfigType_STag
 {
-  uint16 usBaudrateId;
-  uint16 usBaudrateBRP;
-  uint8  ucBaudratePropSeg;
-  uint8  ucBaudrateSeg1;
-  uint8  ucBaudrateSeg2;
-  uint8  ucBaudrateSJW;
-  const  Can_FdBaudrateConfigType *Can_FdBaudrateConf;
-}Can_BaudrateConfigType;
+  const  Can_FdBaudrateConfigType * stCan_FdBaudrateConf;
+  uint16                            usBaudrateId;
+  uint16                            usBaudrateBRP;
+  uint8                             ucBaudratePropSeg;
+  uint8                             ucBaudrateSeg1;
+  uint8                             ucBaudrateSeg2;
+  uint8                             ucBaudrateSJW;
+} Can_BaudrateConfigType;
 
 /* Data structure contain post-compile data:
   ptr to baudrate configuration structure
@@ -37,9 +40,34 @@ typedef struct Can_BaudrateConfigType_STag
   */
 typedef struct Can_ControllerPBConfigType_STag
 {
-  const Can_BaudrateConfigType *Can_BaudrateConf;
-  uint8 ucCanControllerDefaultBaudrate;
-  uint8 ucCanControllerId;
+  const Can_BaudrateConfigType *stCan_BaudrateConf;
+  uint8                         ucCanControllerDefaultBaudrate;
+  uint8                         ucCanControllerId;
 } Can_ControllerPBConfigType;
+
+typedef struct Can_IcomControllerPBConfigType_STag
+{
+  
+} Can_IcomControllerPBConfigType;
+
+typedef struct Can_HwObjectPBConfigType_STag
+{
+  uint16          usObjId;
+  uint16          usObjCount;
+  CanHandleType   enObjHandleType;
+  CanIdType       enObjIdType;
+  CanObjectType   enObjType;
+  uint8           ucObjFdPaddingVal;
+  uint8           ucCanControllerRef;
+  boolean         blObjPolling;
+  boolean         blObjTrigerTx;
+} Can_HwObjectPBConfigType;
+
+typedef struct Can_HwFilterPBConfigType_STag
+{
+  
+} Can_HwFilterPBConfigType;
+
+
 
 #endif /*End of Can_PBTypes.h*/
--- modules/CAN/include/Can_PCTypes.h
+++ modules/CAN/include/Can_PCTypes.h
@@ -8,35 +8,15 @@
 #ifndef CAN_PCTYPES_H
 #define CAN_PCTYPES_H
 
-typedef enum Can_ModeOpType_ETag
-{
-  CAN_INTERRUPT,
-  CAN_POLLING,
-  CAN_MIXED
-} Can_ModeOpType;
+#include "Can_Types.h"
 
-/* Type of CanIcomSignalOperation */
-typedef enum Can_IcomSignalOperationType_ETag
-{
-  CAN_ICOM_SIG_OP_AND = 0u,
-  CAN_ICOM_SIG_OP_EQUAL,
-  CAN_ICOM_SIG_OP_GREATER,
-  CAN_ICOM_SIG_OP_SMALLER,
-  CAN_ICOM_SIG_OP_XOR
-} Can_IcomSignalOperationType;
 
-typedef enum Can_IcomLevelType_ETag
+/* [ref]: ECUC_Can_00444 */
+typedef struct Can_IcomGeneralPCConfigType_STag
 {
-  CAN_ICOM_LEVEL_ONE,
-  CAN_ICOM_LEVEL_TWO
-} Can_IcomLevelType;
-
-typedef enum CanIcomVariantType_ETag
-{
-  CAN_ICOM_VARIANT_NONE,
-  CAN_ICOM_VARIANT_HW,
-  CAN_ICOM_VARIANT_SW
-} CanIcomVariantType;
+  Can_IcomLevelType   enCanIcomLevel;
+  Can_IcomVariantType enCanIcomVariant;
+} Can_IcomGeneralPCConfigType;
 
 /* Data structure contain pre-compile data:
   ref to EcuM wakeup API
@@ -49,12 +29,13 @@ typedef enum CanIcomVariantType_ETag
 /* [ref]: ECUC_Can_00354 */
 typedef struct Can_ControllerPCConfigType_STag
 {
-  EcuM_WakeupSourceType CanWakeupRef;
-  Can_ModeOpType enCanWakeupModeOp;
-  Can_ModeOpType enCanBusoffModeOp;
-  Can_ModeOpType enCanRxModeOp;
-  Can_ModeOpType enCanTxModeOp;
-  uint8 ucCanControllerId;
+  EcuM_WakeupSourceType     enCanWakeupRef;
+  Can_ModeOpType            enCanWakeupModeOp;
+  Can_ModeOpType            enCanBusoffModeOp;
+  Can_ModeOpType            enCanRxModeOp;
+  Can_ModeOpType            enCanTxModeOp;
+  Can_ControllerStateType * ptCanControllerSts;
+  uint8                     ucCanControllerId;
 } Can_ControllerPCConfigType;
 
 /* Data structure contain pre-compile data:
@@ -63,17 +44,10 @@ typedef struct Can_ControllerPCConfigType_STag
 /* [ref]: ECUC_Can_00328 */
 typedef struct Can_GeneralPCConfigType_STag
 {
-  uint8 ucCanIndex;
-  uint8 ucCanMultiplexedTx;
-  uint8 ucCanIcomEnable;
   Can_IcomGeneralPCConfigType * stCanIcomGeneral;
+  uint8                         ucCanIndex;
+  uint8                         ucCanMultiplexedTx;
+  uint8                         ucCanIcomEnable;
 } Can_GeneralPCConfigType;
-
-/* [ref]: ECUC_Can_00444 */
-typedef struct Can_IcomGeneralPCConfigType_STag
-{
-  Can_IcomLevelType CanIcomLevel;
-  CanIcomVariantType CanIcomVariant;
-} Can_IcomGeneralPCConfigType;
 
 #endif /*End of Can_PCTypes.h*/
